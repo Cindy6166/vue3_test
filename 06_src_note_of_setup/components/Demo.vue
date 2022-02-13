bc977b9bc6271748de4d8d1a7b9cce02208a779b
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-13 16:32:10 +0800

    Note of setup()

---
+++ 06_src_note_of_setup/components/Demo.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>information</h1>
+  <h2>Name: {{ person.name }}</h2>
+  <h2>Age: {{ person.age }}</h2>
+  <button @click="test">Trigger Hello event of Demo
+  </button>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "Demo",
+  props:['msg','school'],
+  emits:['hello'],
+//   beforeCreate(){
+//       console.log('---beforeCreate---');
+//   },
+  setup(props,context) {
+    //   console.log('---setup---',props,context);
+    //   console.log('---setup---',context.attrs); //like $attrs in Vue2
+    //   console.log('---setup---',context.emit); //trigger customized event
+      console.log('---setup---',context.slots); //slots
+
+    // data
+    const person = reactive({
+      name: 'Tom',
+      age: 18,
+    })
+
+    //methods
+    function test(){
+       context.emit('hello', 666) 
+    }
+    
+    // return an object
+    return {
+      person,
+      test
+    };
+  },
+};
+</script>
+
