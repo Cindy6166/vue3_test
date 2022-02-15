3ab4f3f542bd91d076e1eb9209b92ddd99418462
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-15 17:23:02 +0800

    feat: usage of hooks

---
+++ 12_src_hooks/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>test</h2>
+  <h2>mouse X: {{ point.x }}, y: {{ point.y }}</h2>
+</template>
+
+<script>
+import usePoint from '../hooks/usePoint'
+export default {
+    name:'Test',
+    setup(){
+        const point = usePoint()
+        return {point}
+    }
+}
+</script>
+
+<style>
+
+</style>
