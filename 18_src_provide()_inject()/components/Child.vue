4bc29b307a1d7ad3898f3852f8bcfc4679897749
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-16 15:50:57 +0800

    feat: usage of provide() and inject()

---
+++ 18_src_provide()_inject()/components/Child.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="child">
+    <h3>Child</h3>
+    <Grandchild/>
+  </div>
+</template>
+
+<script>
+import {inject} from 'vue'
+import Grandchild from './Grandchild'
+export default {
+  name:'Child',
+  components:{Grandchild},
+  setup(){
+      let x = inject('car')
+      console.log(x);
+  }
+}
+</script>
+
+<style>
+.child{
+  background-color: skyblue;
+  padding: 10px;
+}
+
+</style>
---
+++ 18_src_provide()_inject()/components/Grandchild.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grandchild">
+    <h3>Grandchild, {{car.name}} : {{car.price}}</h3>
+  </div>
+</template>
+
+<script>
+import {inject} from 'vue'
+export default {
+  name:'Grandchild',
+  setup(){
+      let car = inject('car')
+
+      return {car}
+  }
+}
+</script>
+
+<style>
+.grandchild{
+  background-color: rosybrown;
+  padding: 10px;
+}
+
+</style>
