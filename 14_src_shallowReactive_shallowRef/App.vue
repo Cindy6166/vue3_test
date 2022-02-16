0d795f05f28aa6e03c95ba0ec18f931c49bc3877
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-02-16 10:40:08 +0800

    feat: usage of shallowReactive and shallowRef

---
+++ 14_src_shallowReactive_shallowRef/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Demo v-if="isShowDemo"/>
+</template>
+
+<script>
+import {ref} from 'vue'
+import Demo from './components/Demo.vue'
+export default {
+  name:'App',
+  components:{Demo},
+  setup(){
+     let isShowDemo = ref(true)
+     return {isShowDemo}
+  }
+}
+</script>
+
+<style>
+
+</style>
